911d47eeffecf30b1c446813f7032d9dfe250f36
danielmassita <daniel.massita@gmail.com>
2022-10-16 23:49:21 -0300

    FimAula06

---
+++ Unidade01-Aula06.js
@@ -0,0 +1,20 @@
+/* 
+Type String
+- usamos pra guardar dados, que não sejam números nem boolean;
+*/
+
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+const senha = `senhaSuperSeguro456!`;
+const stringDeNumeros = "34567"; // cadeia de caracteres, apesar de serem numéricos...
+
+const citacao = 'Meu nome é '; // usamos aspas simples e duplas pra diferenciar uma citação...
+console.log(citacao);
+
+// Template String ou Template Literal (usando crase `)...
+
+// Concatenação (+) pra agregar textos...
+
+const meuNome = "Daniel"
+console.log(citacao + meuNome);
+
