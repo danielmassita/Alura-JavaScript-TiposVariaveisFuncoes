a716d8dc670a4a51d62d2a093e05ab407fc9eea7
danielmassita <daniel.massita@gmail.com>
2022-10-17 00:45:31 -0300

    Fim Unidade 01

---
+++ Unidade01-Aula13.js
@@ -0,0 +1,20 @@
+/*
+13
+Faça como eu fiz: Comportamento dos tipos
+PRÓXIMA ATIVIDADE
+
+Nesta aula, começamos a aprender sobre tipos um conceito inicial e um dos mais importantes em qualquer linguagem. Estudamos com maiores detalhes os tipos primitivos number, string e boolean, mas temos que praticar para internalizar.
+
+VER OPINIÃO DO INSTRUTOR
+Opinião do instrutor
+
+No caso de números, é possível salvar em uma variável não somente o número em si, como em const num = 5;, mas também o resultado de uma operação (ou mais):
+*/
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+/*
+Veja que, no código acima, utilizamos parênteses () para seguir a regra matemática de precedência nas operações.
+
+Você consegue pensar em mais casos com as operações matemáticas básicas para testar?
+*/
