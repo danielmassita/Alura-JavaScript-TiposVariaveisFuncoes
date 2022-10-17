b816c06af6fb7dc249a7a038c379907737a31220
danielmassita <daniel.massita@gmail.com>
2022-10-16 23:38:48 -0300

    FimAula04-TipoNumber

--- Unidade01-Aula04.js
+++ Unidade01-Aula04.js
@@ -6,13 +6,24 @@ Tipos Primitivos
   - Boolean
 */
 
-const meuNumero = 3;
+//TYPE NUMBER 
 
+const meuNumero = 3;
 const primeiroNumero = 1;
 const segundoNumero = 2;
 
 const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log(operacaoMatematica);
 
-console.log(operacaoMatematica)
+// PONTO FLUTUANTE (Float)
 
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
 
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+console.log(novaOperacao);
+
+// NaN (Not a Number)
+
+const alura = 'Alura';
+console.log(alura * primeiroNumero);
