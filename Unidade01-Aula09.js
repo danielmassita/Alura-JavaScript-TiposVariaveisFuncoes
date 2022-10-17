b50650c982b493a0b552ab2af2ded8bb7f8d6161
danielmassita <daniel.massita@gmail.com>
2022-10-17 00:14:32 -0300

    Final da Unidade 01

---
+++ Unidade01-Aula09.js
@@ -0,0 +1,14 @@
+// Type Boolean (true || false) muito usado para comparações lógicas
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+let cadastroAtivado = false;
+cadastroAtivado = true; // Podemos começar com uma variável falsa e somente depois do acesso alterar pra True...
+
+console.log(primeiroNumero === segundoNumero); //false
+console.log(primeiroNumero === 5); //true
+
+const texto1 = "Alura";
+const texto2 = "alura";
+console.log(texto1 === texto2); //false
+
