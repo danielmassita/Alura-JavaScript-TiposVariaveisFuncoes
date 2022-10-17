a39b284761e97888f6b5e2085332d1f39f93e45a
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2022-10-16 23:27:05 -0300

    Iniciando a Aula 04 Type Number

--- Unidade01-Aula04.js
+++ Unidade01-Aula04.js
@@ -5,3 +5,14 @@ Tipos Primitivos
   - Strings
   - Boolean
 */
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+
