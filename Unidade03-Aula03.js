7dc4c6762a70caef5d144eeb873b8558048426bb
danielmassita <daniel.massita@gmail.com>
2022-10-20 13:16:10 -0300

    Unidade 03 Aula 03 Errors

---
+++ Unidade03-Aula03.js
@@ -0,0 +1,15 @@
+// Erros e StackTrace
+
+// const numero; 
+//SYNTAX ERROR, diz que tá faltando inicializar a constante, no caso atribuir valor... 
+const numero = 0;
+
+// console.log(numero
+// SYNTAX ERROR, falta fechar o código ali com o )...
+console.log(numero);
+
+// console.log(minhaVar);
+// REFERENCE ERROR, minhaVar não foi definida, atribuída, nem existe... 
+var minhaVar = "Olá, Mundo!";
+console.log(minhaVar);
+
