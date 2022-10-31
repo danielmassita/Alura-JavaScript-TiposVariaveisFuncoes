0225db5c32458a8367c31cdf474822c0898d39b3
danielmassita <daniel.massita@gmail.com>
2022-10-31 20:11:10 -0300

    Voltando aos estudos!

---
+++ Unidade04-Aula03.js
@@ -0,0 +1,23 @@
+/*
+03
+Para saber mais: Mais sobre operadores
+PRÓXIMA ATIVIDADE
+
+Até agora praticamos com alguns dos operadores do JavaScript, especialmente alguns dos utilizados em operações matemáticas:
+
+nome	operador
+Atribuição de adição	x + y
+Atribuição de subtração	x - y
+Atribuição de multiplicação	x * y
+Atribuição de divisão	x / y
+Começamos com os operadores mais utilizados, pois sempre que aprendemos uma linguagem de programação nova, o melhor é absorvermos mais conhecimentos aos poucos sobre cada assunto, em vez de tentar dominar tudo de uma vez.
+
+Porém, a lista de operadores é extensa e há muitas formas de utilizá-los. Além das operações matemáticas básicas, do = para atribuição de valor em uma variável e das comparações com == e ===, utilizamos operadores para diversas outras tarefas de código, por exemplo:
+
+||: Operador “ou”, retorna true caso uma condição seja válida;
+&&: Operador “e”, retorna true somente se todas as condições forem válidas;
+!= e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
+A documentação do MDN sobre operadores tem a lista completa, com vários exemplos. Invista um tempo para se familiarizar com cada um deles e tente pensar em exemplos de uso para cada um.
+
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators 
+*/
