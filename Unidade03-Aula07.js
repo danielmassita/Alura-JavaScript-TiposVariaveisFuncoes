2dda1baf509fe5e7254d52d5aee1e62762f95296
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2022-10-31 19:36:23 -0300

    Create Unidade03-Aula07.js

---
+++ Unidade03-Aula07.js
@@ -0,0 +1,24 @@
+/*
+07
+Características do JS
+PRÓXIMA ATIVIDADE
+
+O JavaScript foi padronizado em 1996 pela European Computer Manufacturers Association (ECMA), e é por isso que às vezes você o ouve como ECMAScript. É uma linguagem incrivelmente poderosa, presente na maioria dos navegadores e no back-end de grandes sistemas.
+Sabendo disso, analise as alternativas abaixo e marque as verdadeiras sobre esta linguagem de programação.
+
+
+No JavaScript, os tipos de dados podem ser divididos em duas categorias: tipos primitivos e tipos de objetos. CORRETO
+Alternativa correta! Muito bem! Os tipos primitivos do JavaScript incluem números, texto (conhecidas como strings) e valores booleanos. Já o tipo objeto é uma coleção de propriedades, onde cada uma possui um nome e um valor, sendo ele um valor primitivo ou outro objeto.
+
+
+No JavaScript, podemos criar um programa com paradigma funcional e lógico. CORRETO
+Alternativa correta! Certo, podemos sim. O JavaScript é uma linguagem de programação multiparadigma e possui suporte para funcional, orientado a objetos ou lógico por exemplo.
+
+
+Divisão por zero não é um erro no JavaScript. CORRETO
+Alternativa correta! Isso aí. Diferente de outras linguagens de programação como Java ou Python, divisão por zero no JavaScript não gera um erro. O retorno desta operação é infinito ou infinito negativo (Infinity). Porém, a divisão de zero por zero resulta num NaN (not a number).
+
+
+O JavaScript é fortemente tipado, como o Java, por isso a semelhança no nome. ERRADO
+O JavaScript é dinamicamente tipado. Isso significa que os tipos não são declarados no código e, portanto, conhecidos/checados em tempo de execução. Não se deve confundir o JavaScript com a linguagem de programação Java. Tanto Java quanto JavaScript são marcas registradas da Oracle nos Estados Unidos da América e em outros países. No entanto, as duas linguagens de programação possuem sintaxe, semânticas e usos muito diferentes.
+*/
